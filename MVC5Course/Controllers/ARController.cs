f988fdaf9ce7198318b4859f4c01cd587b38b139
HLC <s93406022@gmail.com>
2017-03-18 14:13:48 +0800

    使用 FileResult 回傳任意檔案

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -11,7 +11,28 @@ namespace MVC5Course.Controllers
         // GET: AR
         public ActionResult Index()
         {
+            return View("123");
+        }
+
+        public ActionResult View2()
+        {
+            return PartialView("Index");
+        }
+
+        public ActionResult View3()
+        {
             return View();
+        }
+
+        public ActionResult File1()
+        {
+            //return File(@"C:\Projects\MVC5Course\MVC5Course\Content\J.jpg", "image/jpeg");
+            return File(Server.MapPath("~/Content/J.jpg"), "image/jpeg");
+        }
+
+        public ActionResult File2()
+        {
+            return File(Server.MapPath("~/Content/J.jpg"), "image/jpeg","喬巴.jpg");
         }
     }
 }
